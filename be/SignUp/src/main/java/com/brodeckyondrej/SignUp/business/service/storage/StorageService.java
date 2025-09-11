14020e3102136a7617fe5a50ef6cf77e718cb7f8
Ondřej Brodecký <536513@muni.cz>
2025-09-11 14:53:42 +0200

    add StorageService interface and StorageException

---
+++ be/SignUp/src/main/java/com/brodeckyondrej/SignUp/business/service/storage/StorageService.java
@@ -0,0 +1,32 @@
+package com.brodeckyondrej.SignUp.business.service.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+
+    /**
+     * @param file file to store
+     * @return filename
+     */
+    String store(MultipartFile file);
+
+    /**
+     * @param filename
+     * @return returns file as Resource or throws MissingObjectException
+     */
+    Resource load(String filename);
+
+    /**
+     * Deletes file with given filename. If none is found MissingObjectException is not thrown
+     * @param filename
+     */
+    void delete(String filename);
+
+    /**
+     * Replaces file with given filename with new file
+     * @param filename
+     * @param file
+     */
+    void replace(String filename, MultipartFile file);
+}
---
+++ be/SignUp/src/main/java/com/brodeckyondrej/SignUp/exception/StorageException.java
@@ -0,0 +1,7 @@
+package com.brodeckyondrej.SignUp.exception;
+
+public class StorageException extends RuntimeException {
+    public StorageException(String message) {
+        super(message);
+    }
+}
