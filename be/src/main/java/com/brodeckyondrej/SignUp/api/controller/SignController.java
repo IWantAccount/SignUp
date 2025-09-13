7cb98afa7262efd98a13f72a36672cb625f804c2
Ondřej Brodecký <536513@muni.cz>
2025-09-13 17:15:05 +0200

    feat: sign paging

--- be/src/main/java/com/brodeckyondrej/SignUp/api/controller/SignController.java
+++ be/src/main/java/com/brodeckyondrej/SignUp/api/controller/SignController.java
@@ -8,24 +8,41 @@ import com.brodeckyondrej.SignUp.business.dto.sign.UpdateSignDto;
 import com.brodeckyondrej.SignUp.business.service.sign.SignService;
 import com.brodeckyondrej.SignUp.persistence.entity.Sign;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+
+import java.util.UUID;
+
 
 @RestController()
 @RequestMapping("/sign")
 public class SignController extends EntityController<Sign, CreateSignDto, UpdateSignDto, SignGetDetailDto, SignGetListDto> {
 
+    private final SignService signService;
+
     public SignController(SignService service){
         super(service);
+        this.signService = service;
     }
 
     @Override
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<SignGetDetailDto> create(@Valid @ModelAttribute CreateSignDto createSignDto) {
         return super.create(createSignDto);
+    }
+
+    @GetMapping("/by-category/{categoryId}")
+    public ResponseEntity<Page<SignGetListDto>> getByCategoryId(@PathVariable UUID categoryId, Pageable pageable) {
+        Page<SignGetListDto> result = signService.getByCategoryId(categoryId, pageable);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/by-private-collection/{collectionId}")
+    public ResponseEntity<Page<SignGetListDto>> getByCollectionId(@PathVariable UUID collectionId, Pageable pageable) {
+        Page<SignGetListDto> result = signService.getByPrivateCollectionId(collectionId, pageable);
+        return ResponseEntity.ok(result);
     }
 }
--- be/src/main/java/com/brodeckyondrej/SignUp/business/service/sign/SignService.java
+++ be/src/main/java/com/brodeckyondrej/SignUp/business/service/sign/SignService.java
@@ -5,14 +5,46 @@ import com.brodeckyondrej.SignUp.business.dto.sign.SignGetDetailDto;
 import com.brodeckyondrej.SignUp.business.dto.sign.SignGetListDto;
 import com.brodeckyondrej.SignUp.business.dto.sign.UpdateSignDto;
 import com.brodeckyondrej.SignUp.business.service.universal.EntityService;
+import com.brodeckyondrej.SignUp.persistence.entity.Category;
+import com.brodeckyondrej.SignUp.persistence.entity.PrivateCollection;
 import com.brodeckyondrej.SignUp.persistence.entity.Sign;
+import com.brodeckyondrej.SignUp.persistence.repository.CategoryRepository;
+import com.brodeckyondrej.SignUp.persistence.repository.PrivateCollectionRepository;
 import com.brodeckyondrej.SignUp.persistence.repository.SignRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
+@Transactional
 public class SignService extends EntityService<Sign, CreateSignDto, UpdateSignDto, SignGetDetailDto, SignGetListDto> {
 
-    public SignService(SignRepository repository, SignValidator validator, SignMapper mapper) {
+    private final SignRepository signRepository;
+    private final SignMapper signMapper;
+    private final PrivateCollectionRepository collectionRepository;
+    private final CategoryRepository categoryRepository;
+
+    public SignService(SignRepository repository, SignValidator validator, SignMapper mapper,
+                       PrivateCollectionRepository collectionRepository, CategoryRepository categoryRepository) {
         super(repository, validator, mapper);
+        this.signRepository = repository;
+        this.signMapper = mapper;
+        this.collectionRepository = collectionRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Page<SignGetListDto> getByCategoryId(UUID categoryId, Pageable pageable){
+        Category category = categoryRepository.findByIdOrThrow(categoryId);
+        return signRepository.findByCategory(category, pageable)
+                .map(signMapper::toDetailDto);
+    }
+
+    public Page<SignGetListDto> getByPrivateCollectionId(UUID privateCollectionId, Pageable pageable){
+        PrivateCollection collection = collectionRepository.findByIdOrThrow(privateCollectionId);
+        return signRepository.findDistinctByInPrivateCollectionsContains(collection, pageable)
+                .map(signMapper::toDetailDto);
     }
 }
