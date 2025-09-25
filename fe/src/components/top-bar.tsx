1aab3eb09c278ee0b246c6ea48a554188e781449
Ondřej Brodecký <536513@muni.cz>
2025-09-25 20:05:54 +0200

    feat: top bar hides on scroll

--- fe/src/components/top-bar.tsx
+++ fe/src/components/top-bar.tsx
@@ -1,26 +1,52 @@
-import {Toolbar, AppBar, Button, IconButton, Box} from "@mui/material";
-//TODO jak nastavit barvy? Co search app bar?
-export function TopBar(){
+import {Toolbar, AppBar, Button, IconButton, Box, Slide, useScrollTrigger, CssBaseline} from "@mui/material";
+import React from "react";
+
+// Většinu kódu jsem převzal z oficiální dokumentace MUI: https://mui.com/material-ui/react-app-bar/
+interface Props {
+    window?: () => Window;
+    children?: React.ReactElement<unknown>;
+}
+
+function HideOnScroll(props: Props) {
+    const {children, window} = props;
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+    });
+
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton
-                edge="start"
-                href={"/"}
-                aria-labe="domů">
-                    <Box
-                    component="img"
-                    src="../../public/neslhk_logo.png"
-                    alt="Neslhk logo"
-                    sx={{ height: 40 }}>
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children ?? <div/>}
+        </Slide>
+    );
+}
 
-                    </Box>
-                </IconButton>
+export function TopBar(props: Props){
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <HideOnScroll {...props}>
+                <AppBar position="static">
+                    <Toolbar sx={{ display: 'flex', justifyContent: 'left', flexWrap: 'wrap' }}>
+                        <IconButton
+                            edge="start"
+                            href={"/"}
+                            aria-labe="domů">
+                            <Box
+                                component="img"
+                                src="../../public/neslhk_logo.png"
+                                alt="Neslhk logo"
+                                sx={{ height: 40 }}>
 
-                <Button color="inherit">Účet</Button>
-                <Button color="inherit">Nápověda</Button>
-                <Button color="inherit">Tři</Button>
-            </Toolbar>
-        </AppBar>
-    )
+                            </Box>
+                        </IconButton>
+                        <Button color="inherit">Účet</Button>
+                        <Button color="inherit">Nápověda</Button>
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+            <Toolbar />
+        </React.Fragment>
+    );
+
+
 }
