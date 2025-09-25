05671247164f7910929101f3a53b1dabd9dc544c
Ondřej Brodecký <536513@muni.cz>
2025-09-25 19:51:36 +0200

    feat: use school logo as home button

--- fe/src/components/top-bar.tsx
+++ fe/src/components/top-bar.tsx
@@ -1,9 +1,22 @@
-import {Toolbar, AppBar, Button} from "@mui/material";
+import {Toolbar, AppBar, Button, IconButton, Box} from "@mui/material";
 //TODO jak nastavit barvy? Co search app bar?
 export function TopBar(){
     return (
         <AppBar position="static">
             <Toolbar>
+                <IconButton
+                edge="start"
+                href={"/"}
+                aria-labe="domů">
+                    <Box
+                    component="img"
+                    src="../../public/neslhk_logo.png"
+                    alt="Neslhk logo"
+                    sx={{ height: 40 }}>
+
+                    </Box>
+                </IconButton>
+
                 <Button color="inherit">Účet</Button>
                 <Button color="inherit">Nápověda</Button>
                 <Button color="inherit">Tři</Button>
