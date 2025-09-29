06487fd708d627849f24f81c48f4a6f3fc63fb19
Ondřej Brodecký <536513@muni.cz>
2025-09-29 17:39:08 +0200

    add category form

---
+++ fe/src/components/forms/category-form.tsx
@@ -0,0 +1,100 @@
+import {z} from "zod";
+import {Controller, type SubmitHandler, useForm} from "react-hook-form";
+import {Autocomplete, Box, Button, TextField} from "@mui/material";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const schema = z.object({
+    name: z.string().trim().min(1, "Název kategorie je povinný").max(100, "Název kategorie může mít maximálně 100 znaků"),
+    subjectId: z.string().min(1, "Předmět je povinný"),
+})
+
+export type CategoryFormData = z.infer<typeof schema>;
+
+interface SubjectNameId {
+    subjectName: string;
+    subjectId: string;
+}
+
+interface Props {
+    defaultName?: string;
+    defaultSubjectId?: string;
+    defaultSubjectName?: string;
+    subjects: SubjectNameId[];
+    onSubmit: SubmitHandler<CategoryFormData>;
+    submitText: string;
+}
+
+export function CategoryForm(props: Props) {
+
+    const options: SubjectNameId[] = [
+        {subjectName: "ČZJ1", subjectId: "1"},
+        {subjectName: "ČZJ2", subjectId: "2"},
+        {subjectName: "ČZJ konverzace 1", subjectId: "3"},
+        {subjectName: "ČZJ konverzace 2", subjectId: "4"},
+    ]
+
+    const {control, handleSubmit} = useForm<CategoryFormData>({
+        resolver: zodResolver(schema),
+        mode: "all",
+        defaultValues: {
+            name: props.defaultName || "",
+            subjectId: props.defaultSubjectId || "",
+        }
+    });
+
+    return (
+        <Box    component="form"
+                onSubmit={handleSubmit(props.onSubmit)}
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 2,
+                    px: 5,
+                    py: 2,
+                    maxWidth: 500,
+                    mx: "auto",
+                    width: "100%",
+                    boxSizing: "border-box",
+                }}>
+
+            <Controller name="name"
+                        control={control}
+                        render={({field, fieldState}) => (
+                            <TextField {...field}
+                                        label="Název kategorie"
+                                        error={!!fieldState.error}
+                                        helperText={fieldState.error?.message}/>
+                        )}
+            />
+
+            <Controller
+                name="subjectId"
+                control={control}
+                render={({ field, fieldState }) => (
+                    <Autocomplete<SubjectNameId, false, false, false>
+                        options={options}
+                        getOptionLabel={(o: SubjectNameId) => o.subjectName}
+                        isOptionEqualToValue={(a: SubjectNameId, b: SubjectNameId) => a.subjectId === b.subjectId}
+                        value={options.find(o => o.subjectId === field.value) ?? null}
+                        onChange={(_, newVal) => field.onChange(newVal?.subjectId ?? "")}
+                        onBlur={field.onBlur}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                inputRef={field.ref}
+                                label="Předmět"
+                                error={!!fieldState.error}
+                                helperText={fieldState.error?.message}
+                            />
+                        )}
+                        noOptionsText="Žádné položky"
+                    />
+                )}
+            />
+
+            <Button variant="contained" type="submit">{props.submitText}</Button>
+
+        </Box>
+    )
+
+}
--- fe/src/routes/app/categories/create.tsx
+++ fe/src/routes/app/categories/create.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {CategoryForm} from "@/components/forms/category-form.tsx";
+import {Box, Typography} from "@mui/material";
 
 export const Route = createFileRoute('/app/categories/create')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/app/categories/create"!</div>
+  return (
+      <Box sx={{display: "flex", flexDirection: "column", gap: 2, p: 2, alignItems: "center"}}>
+        <Typography variant="h4">Vytvořit novou kategorii</Typography>
+        <CategoryForm subjects={[]} onSubmit={() => {}} submitText={"Uložit"}/>
+      </Box>
+  )
 }
