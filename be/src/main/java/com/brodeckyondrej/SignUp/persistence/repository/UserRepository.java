2049b9cddf07ab3a1bf05f4134006791f95da080
Ondřej Brodecký <536513@muni.cz>
2025-11-17 22:37:07 +0100

    refactor: remove unused methods in UserRepository

--- be/src/main/java/com/brodeckyondrej/SignUp/persistence/repository/UserRepository.java
+++ be/src/main/java/com/brodeckyondrej/SignUp/persistence/repository/UserRepository.java
@@ -1,13 +1,6 @@
 package com.brodeckyondrej.SignUp.persistence.repository;
 import com.brodeckyondrej.SignUp.persistence.entity.User;
-import com.brodeckyondrej.SignUp.persistence.enumerated.UserRole;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 
 public interface UserRepository extends NamedEntityRepository<User> {
-
-    Page<User> findByNameContainingIgnoreCaseAndRole(String name, UserRole role, Pageable pageable);
-
-    Page<User> findByRole(UserRole role, Pageable pageable);
 }
