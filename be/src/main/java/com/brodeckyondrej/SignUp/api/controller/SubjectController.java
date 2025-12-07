3114b68b911959a8cda1f06d3e8a992be23f18c1
Ondřej Brodecký <536513@muni.cz>
2025-12-07 12:27:25 +0100

    add authorize annotations

--- be/src/main/java/com/brodeckyondrej/SignUp/api/controller/SubjectController.java
+++ be/src/main/java/com/brodeckyondrej/SignUp/api/controller/SubjectController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +25,6 @@ public class SubjectController extends NamedEntityController<Subject, SubjectCre
         this.subjectService = service;
     }
 
-    //@PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add-student")
     public ResponseEntity<Void> addStudentToSubject(@Valid @RequestBody SubjectStudentDto dto){
         subjectService.addStudent(dto);
