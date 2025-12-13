ac81a7d447d9f00bab1b997cb519ccb05b2b1b03
Ondřej Brodecký <536513@muni.cz>
2025-12-13 11:15:24 +0100

    specify profile for cors config

--- be/src/main/java/com/brodeckyondrej/SignUp/security/DevCorsConfig.java
+++ be/src/main/java/com/brodeckyondrej/SignUp/security/DevCorsConfig.java
@@ -1,18 +1,21 @@
 package com.brodeckyondrej.SignUp.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
-
+@Configuration
+@Profile("local")
 public class DevCorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // FE origin
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
